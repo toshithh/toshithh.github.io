a3a7a6373d25b34ac02318009c8e6a0f9e34e533
Toshith <45565316+toshithh@users.noreply.github.com>
2022-10-18 10:40:38 +0530

    Portfolio

--- m_design.js
+++ m_design.js
@@ -7,6 +7,12 @@ function replaceClass(id, oldClass, newClass) {
     elem.addClass(newClass);
 }
 
+function load_disp(id, w){
+    idh = "#"+id;
+    document.getElementById(id).style.width = "0%";
+    $(idh).animate({width: w}, 1100);
+}
+
 function rightOut(id){
     idh = "#"+id;
     $(idh).animate({right: "-100%"}, def_time*2);
@@ -98,5 +104,48 @@ $(document).ready(function(){
         replaceClass("project", "current", "top-button");
         replaceClass("contact", "top-button", "current");
         an2contact();
+    })
+    //
+    $("#webdev").click(function(){
+        if($("#webdiv").css('display')=='none'){ 
+            load_disp("wd-py", "100%");
+            load_disp("wd-php", "80%");
+            load_disp("wd-html", "90%");
+            load_disp("wd-css", "96%");
+            load_disp("wd-js","90%");
+            load_disp("wd-jq","98%");
+            load_disp("wd-ajax","70%");
+            load_disp("wd-sql", "95%");
+        }
+        $("#webdiv").slideToggle(500);
+    })
+    $("#OS").click(function(){
+        if($("#OS-div").css('display')=='none'){
+            load_disp("os-py", "90%");
+            load_disp("os-bash", "70%");
+        }
+        $("#OS-div").slideToggle(500);
+    })
+    $("#ML").click(function(){
+        if($("#ML-div").css('display')=='none'){
+            load_disp("ml-py", "100%");
+            load_disp("ml-cv", "80%");
+        }
+        $("#ML-div").slideToggle(500);
+    })
+    $("#DS").click(function(){
+        if($("#DS-div").css('display')=='none'){
+            load_disp("ds-pandas", "100%");
+            load_disp("ds-numpy", "100%");
+            load_disp("ds-sql", "95%");
+        }
+        $("#DS-div").slideToggle(500);
+    })
+    $("#others").click(function(){
+        if($("#others-div").css('display')=='none'){
+            load_disp("ot-kotlin", "90%");
+            load_disp("ot-c", "60%");
+        }
+        $("#others-div").slideToggle(500);
     })
 })
